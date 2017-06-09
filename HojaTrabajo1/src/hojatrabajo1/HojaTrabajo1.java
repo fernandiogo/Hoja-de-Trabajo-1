ab4fc78881c1ff80ff6249006d6a41cb0ae38c64
Laptop-PC\Laptop <diegodfuz30@gmail.com>
2017-06-08 20:44:46 -0600

    ejemplo

---
+++ HojaTrabajo1/src/hojatrabajo1/HojaTrabajo1.java
@@ -0,0 +1,12 @@
+
+package hojatrabajo1;
+
+public class HojaTrabajo1 {
+
+    
+    public static void main(String[] args) {
+        interfaz nueva=new interfaz();
+        nueva.setVisible(true);
+    }
+    
+}
